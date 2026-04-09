87d2fb9df9193fc5289a9f4c54c3199fde4a44d0
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-04-09 01:38:03 -0300

    10: Rendering Lists with Signals and @for

--- src/app/products/products-grid/products-grid.ts
+++ src/app/products/products-grid/products-grid.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductCard } from "../product-card/product-card";
+import { Product } from '../product';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-products-grid',
-  imports: [ProductCard],
+  imports: [ProductCard, MatIconModule],
   templateUrl: './products-grid.html',
   styleUrl: './products-grid.scss',
 })
-export class ProductsGrid {}
+export class ProductsGrid {
+
+  protected readonly products = signal<Product[]>([
+    {
+      id: 1,
+      name: 'Cafeteira elétrica',
+      description: 'Cafeteira automática com reservatório de 1,5L e botão de preparo rápido.',
+      price: 249.90,
+      originalPrice: 299.90,
+    },
+    {
+      id: 2,
+      name: 'Fone de ouvido sem fio',
+      description: 'Fone Bluetooth com cancelamento de ruído e até 20 horas de bateria.',
+      price: 179.90,
+      originalPrice: 219.90,
+    },
+    {
+      id: 3,
+      name: 'Mochila urbana',
+      description: 'Mochila resistente, com compartimento para notebook de 15,6" e bolsos organizadores.',
+      price: 169.90,
+    },
+    /* {
+      id: 4,
+      name: 'Smartphone com câmera tripla',
+      description: 'Smartphone 128GB com câmera tripla, tela AMOLED e carregamento rápido.',
+      price: 1299.00,
+      originalPrice: 1399.00,
+    }, */
+  ]);
+}
